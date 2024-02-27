bcceba02d76e59c69c72ecfaf54eb35b7d4a0869
Edvardsn <petter-edvardsen@hotmail.com>
2024-02-27 12:51:03 +0100

    Refactoring of application structure.

--- main.py
+++
@@ -1,9 +0,0 @@
-"""
-This script serves as an application for utilizing images and masks to create a semantic segmentation model based upon given specifications. 
-
-"""
-
-# * Hyperparameters
-img_size = (512, 512)
-num_classes = 4
-batch_size = 32
