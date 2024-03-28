d5621716c6ee25c0d3314f1cd83a0b4e013dcc0c
Petter Edvardsen <pedvardt@gmail.com>
2024-03-28 14:34:55 +0100

    Evaluate model template.

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,4 +1,3 @@
-# from cv2 import applyColorMap
 import os
 import sys
 # Imports the root directory to the path in order to import project modules
@@ -99,32 +98,3 @@ model.fit(
     verbose=2,
 )
 
-# predictions = model.predict(test_dataset)
-# mask = np.argmax(predictions[1], axis=-1)
-
-# Initialize the MeanIoU metric
-miou_metric = keras.metrics.MeanIoU(num_classes=NUM_CLASSES)
-
-# Iterate over the test dataset
-for images, true_masks in test_dataset:
-    # Predict segmentation masks
-    pred_masks_probs = model.predict(images)
-    crf_mask = conditional_random_field(
-        image=images.numpy(), pred_mask_probs=pred_masks_probs, inference_iterations=5
-    )
-
-    # Convert predictions from probabilities to class indices if necessary
-    pred_masks = tf.argmax(pred_masks_probs, axis=-1)
-    
-    # Ensure true_masks and pred_masks are compatible with MeanIoU requirements
-    # This might require casting the dtype and reshaping if necessary
-    pred_masks = tf.cast(pred_masks, dtype=tf.int32)
-    true_masks = tf.cast(true_masks, dtype=tf.int32)
-    
-    # Update the MeanIoU metric
-    miou_metric.update_state(true_masks, pred_masks)
-
-# Retrieve the final mean IoU value
-mean_iou = miou_metric.result().numpy()
-
-print(f"Mean IoU over the test set: {mean_iou}")
